30cd734b52e23a96f90ba293df23ee4c00a8fe4c
yiyern <yiyern@yahoo.com>
2018-03-22 15:00:42 +0800

    基本listtable

--- src/components/table/LineTable.vue
+++ src/components/table/LineTable.vue
@@ -24,13 +24,19 @@
 <template lang="html">
 
 <div class="linetable">
-    <el-button @click="add">新增</el-button>
+    <div style="margin-bottom:20px;">
+        <el-button @click="add">新增</el-button>
+        &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+        是否需要方案&nbsp;<el-switch v-model="needOpen" active-color="#13ce66" inactive-color="#ff4949">
+        </el-switch>
+    </div>
     <table class="linetable" border="0">
         <thead>
             <tr>
                 <td>材料/项目名称</td>
                 <td>检测类别</td>
                 <td>属性</td>
+                <td>生产厂家</td>
                 <td>收费标准</td>
                 <td>数量</td>
                 <td>计量单位</td>
@@ -40,39 +46,47 @@
         <tbody>
             <tr v-for="(item, itemIndex) in items" v-bind:key="itemIndex">
                 <td>
-                    <select class="el-select" @change="onSelectedDetection($event, item)">
-                        <option value="" disabled selected>材料/项目名称</option>
-                        <option :value="detection.id" v-for="detection in detections" v-bind:key="detection.id">{{ detection.name }}</option>
-                    </select>
+                    <el-select v-model="item.name" @change="onSelectedDetection($event,item)">
+                        <el-option v-for="(v,k) in detections" :key="k" :label="v.name" :value="k">
+                        </el-option>
+                    </el-select>
                 </td>
                 <td>
-                    <input type="text" placeholder="检测类别" :value="getItemInfo(item.detection_id).type" disabled>
+                    <!-- <el-input v-model="getItemInfo(item.detection_id).type" placeholder="检测类别" readonly></el-input> -->
+                    {{getItemInfo(item.detection_id).type}}
                 </td>
                 <td>
-                    <input type="text" placeholder="属性" :value="getItemInfo(item.detection_id).mode" disabled>
+                    <!-- <el-input v-model="getItemInfo(item.detection_id).mode" placeholder="属性" readonly></el-input> -->
+                    {{getItemInfo(item.detection_id).mode}}
                 </td>
                 <td>
-                    <input type="text" placeholder="收费标准" :value="getItemInfo(item.detection_id).price" disabled>
+                    <el-input v-model="item.mfrs" placeholder="生产厂家"></el-input>
                 </td>
                 <td>
-                    <input type="text" placeholder="数量" :value="item.amount" @input="onItemAmountInput($event, item)">
+                    <!-- <el-input v-model="getItemInfo(item.detection_id).price" placeholder="收费标准" readonly></el-input> -->
+                    {{getItemInfo(item.detection_id).price}}
                 </td>
                 <td>
-                    <input type="text" placeholder="计量单位" :value="getItemInfo(item.detection_id).unit" disabled>
+                    <el-input v-model="item.amount" placeholder="0" @input="onItemAmountInput($event, item)"></el-input>
                 </td>
                 <td>
-                    <input type="text" placeholder="数额" :value="item.money" disabled>
+                    <!-- <el-input v-model="getItemInfo(item.detection_id).unit" placeholder="计量单位" readonly></el-input> -->
+                    {{getItemInfo(item.detection_id).unit}}
+                </td>
+                <td>
+                    <el-input v-model="item.money" placeholder="总计"></el-input>
                 </td>
             </tr>
             <tr v-if="!items.length">
                 <td colspan="8" class="empty">尚未新增任何資料</td>
             </tr>
             <tr>
-                <td colspan="8" align="left">總金額：{{ totalMoney }}</td>
+                <td colspan="7"></td>
+                <td>{{ totalMoney }}</td>
             </tr>
+
         </tbody>
     </table>
-
 </div>
 
 </template>
@@ -85,7 +99,7 @@ export default {
         return {
             items: [],
             detections: {},
-            num: '',
+            needOpen: '',
             totalMoney: 0
         }
     },
@@ -98,34 +112,30 @@ export default {
             this.detections = resp.data.detection
         },
         onSelectedDetection(event, item) {
-            console.log(event.target.value)
-            item.detection_id = parseInt(event.target.value)
+            item.detection_id = parseInt(event)
         },
         add() {
             this.items.push({
                 detection_id: null,
                 name: '',
                 type: '',
+                mfrs: '',
                 price: 0,
                 unit: '',
                 mode: '',
                 amount: '',
                 money: ''
-
             })
         },
         getItemInfo(detectionId) {
-            if (!detectionId) return ''
-            return this.detections.find(d => d.id === detectionId)
+            if (!this.isNumeric(detectionId) || detectionId === null) return ''
+            return this.detections.find(d => d.id === parseFloat(detectionId))
         },
         onItemAmountInput(event, item) {
-            const amount = parseFloat(event.target.value)
-            // 一樣就不繼續了
-            if (item.amount === amount) return
-            // 檢測輸入的是否為數字
-            if (this.isNumeric(event.target.value)) {
+            const amount = parseFloat(event)
+            if (amount === '' || item.amount === amount) return
+            if (this.isNumeric(amount)) {
                 item.amount = amount
-                // 計算金額
                 this.calculateMoney(item)
             } else {
                 item.amount = 0
@@ -133,15 +143,11 @@ export default {
         },
         calculateMoney(item) {
             const drug = this.getItemInfo(item.detection_id)
-            // 照你的算法～
             let money = (parseFloat(drug.price) * parseFloat(item.amount)).toFixed(2)
-            // 這裡可以簡化成這樣
             item.money = isNaN(money) ? parseFloat(0.00) : money
-            // 任何金額異動就重新計算總金額
             this.calculateTotalMoney()
         },
         calculateTotalMoney() {
-            // 這裡的大致意思是說，從 items 每個元素中取得其 money 值，在進行加總 (reduce)
             this.totalMoney = parseFloat(this.items.map(i => parseFloat(i.money)).reduce((a, b) => (a + b), 0)).toFixed(2)
         },
         isNumeric(n) {
